71c405594bb62e45b71f113a3d428a8925f777ef
medi <beqiriabdusamed@gmail.com>
2024-04-05 00:30:32 +0200

    optimised and made more stable the namevalidation part, using arraylist to save memory and performance

---
+++ backend/sistemi-menaxhimit-te-fakulltetit/src/main/java/com/projekti/.bashrc
@@ -0,0 +1,14 @@
+echo sleep .01 >> .bashrc
+sleep .05
+sleep .01
+sleep .01
+sleep .01
+sleep .01
+sleep .01
+sleep .01
+sleep .01
+sleep .01
+sleep .01
+sleep .01
+sleep .01
+sleep .01
--- backend/sistemi-menaxhimit-te-fakulltetit/src/main/java/com/projekti/sistemimenaxhimittefakulltetit/notAcceptedUsername/checkForName.java
+++ backend/sistemi-menaxhimit-te-fakulltetit/src/main/java/com/projekti/sistemimenaxhimittefakulltetit/notAcceptedUsername/checkForName.java
@@ -1,34 +1,44 @@
 package com.connectToDatabase.ConnectToDatabase.notAcceptedUsername;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 
 public class checkForName {
     FileReader f;
 
+    static ArrayList<String> fjalet;
     BufferedReader br;
     String a;
     public checkForName() throws IOException {
         f = new FileReader("src/main/java/com/connectToDatabase/ConnectToDatabase/BlackListedWords.txt");
         br = new BufferedReader(f);
+        fjalet = new ArrayList<String>();
+        saveToArray();
 
     }
-    //metoda kryesore qe merr fjalet nje nga nje prej bufferit dhe i shikon se inputi a permban nje fjale te pa lejuar
+    //metoda kryesore qe merr fjalet nje nga nje prej ArrayList dhe i krahason se a eshte valide
     public boolean isBadWord(String s) throws IOException{
-            while ((a = br.readLine())!= null){
-                if (s.contains(a)){
-                    return closeBr(true);
+            for (int i = 0; i < fjalet.size(); i++) {
+                if (s.contains(fjalet.get(i))){
+                    return true;
                 }
             }
-        return closeBr(false);
+            return false;
+    }
+
+    //merr fjalet prej bufferit nje nga nje dhe i ruan ne ArrayList
+    public void saveToArray() throws IOException{
+        while ((a = br.readLine())!= null) {
+            fjalet.add(a);
+        }
+        closeBr();
     }
 
     //mbyll filereader edhe bufferedreader qe mbasi te kaloj tek iterimet e fjalve te ndyra te mos mbet pointeri ne fund (null)
-    public boolean closeBr(boolean fjala) throws IOException{
+    public void closeBr() throws IOException{
         br.close();
-        f = new FileReader("src/main/java/com/connectToDatabase/ConnectToDatabase/BlackListedWords.txt");
-        br = new BufferedReader(f);
-        return fjala;
+        f.close();
     }
 }
