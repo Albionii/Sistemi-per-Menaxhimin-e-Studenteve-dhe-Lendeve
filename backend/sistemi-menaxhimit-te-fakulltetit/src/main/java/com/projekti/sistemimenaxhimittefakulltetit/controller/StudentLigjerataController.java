cc7c7d0565607a8f86c70913226146f4e6ec2d94
DrenSylaj <ds68258@ubt-uni.net>
2024-04-08 23:00:12 +0200

    Perqindjet dhe mesataret e notave
    Shtimi i disa funksioneve shtese. Si gjetja e mesatareve per cdo note per studentin specifik, si dhe mesataren totale. Gjithashtu edhe disa funksione shtese ne lidhje me CRUD.

--- backend/sistemi-menaxhimit-te-fakulltetit/src/main/java/com/projekti/sistemimenaxhimittefakulltetit/controller/StudentLigjerataController.java
+++ backend/sistemi-menaxhimit-te-fakulltetit/src/main/java/com/projekti/sistemimenaxhimittefakulltetit/controller/StudentLigjerataController.java
@@ -3,6 +3,7 @@ package com.projekti.sistemimenaxhimittefakulltetit.controller;
 import com.projekti.sistemimenaxhimittefakulltetit.entities.Student;
 import com.projekti.sistemimenaxhimittefakulltetit.entities.StudentLigjerata;
 import com.projekti.sistemimenaxhimittefakulltetit.entities.User;
+import com.projekti.sistemimenaxhimittefakulltetit.entities.Vleresimi;
 import com.projekti.sistemimenaxhimittefakulltetit.service.StudentLigjerataService;
 import com.projekti.sistemimenaxhimittefakulltetit.service.StudentService;
 import com.projekti.sistemimenaxhimittefakulltetit.service.UserService;
@@ -12,6 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -35,6 +37,11 @@ public class StudentLigjerataController {
         return vleresimiService.notaMesatare(id);
     }
 
+    @GetMapping("/notat/{id}")
+    public List<Vleresimi> getAllNotat(@PathVariable Long id){
+        return vleresimiService.getAllNotat(id);
+    }
+
     @DeleteMapping("{id}")
     public void refuzoNoten(@PathVariable Long id){
         vleresimiService.refuzoNoten(id);
@@ -52,5 +59,11 @@ public class StudentLigjerataController {
         StudentLigjerata sl = studentLigjerataService.enroll(id, student);
         return new ResponseEntity<>(sl, HttpStatus.OK);
     }
+
+    @GetMapping("mesataret/{id}")
+    public List<Double[]> mesataretPerNote(@PathVariable Long id){
+        return Collections.singletonList(vleresimiService.mesateretPerNote(id));
+    }
+
 
 }
--- backend/sistemi-menaxhimit-te-fakulltetit/src/main/java/com/projekti/sistemimenaxhimittefakulltetit/service/VleresimiService.java
+++ backend/sistemi-menaxhimit-te-fakulltetit/src/main/java/com/projekti/sistemimenaxhimittefakulltetit/service/VleresimiService.java
@@ -1,8 +1,12 @@
 package com.projekti.sistemimenaxhimittefakulltetit.service;
 
+import com.projekti.sistemimenaxhimittefakulltetit.entities.Vleresimi;
 import com.projekti.sistemimenaxhimittefakulltetit.repository.VleresimiRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,7 +18,42 @@ public class VleresimiService {
         return vleresimiRepository.findAverageNotaByStudentId(studentId);
     }
 
+    //Create
+    public Vleresimi addNota(Vleresimi vleresimi){
+        return vleresimiRepository.save(vleresimi);
+    }
+
+    //Delete
     public void refuzoNoten(Long vleresimiId){
         vleresimiRepository.deleteById(vleresimiId);
+    }
+
+    //Read
+    public List<Vleresimi> getAllNotat(Long studentId){
+        return vleresimiRepository.getAllNotat(studentId);
+    }
+
+    //Update
+    public Optional<Vleresimi> updateNota(Vleresimi updatedVleresimi, Long oldVleresimiId) {
+        Optional<Vleresimi> oldNotaOptional = vleresimiRepository.findById(oldVleresimiId);
+
+        if (oldNotaOptional.isPresent()) {
+            Vleresimi oldNota = oldNotaOptional.get();
+            oldNota.setNota(updatedVleresimi.getNota());
+            oldNota.setDataVendosjes(updatedVleresimi.getDataVendosjes());
+            vleresimiRepository.save(oldNota);
+        }
+
+        return oldNotaOptional;
+    }
+
+    public Double[] mesateretPerNote(Long id) {
+        List<Double[]> notatPerqindjet = vleresimiRepository.mesataretPerNote(id);
+
+        if (notatPerqindjet != null && !notatPerqindjet.isEmpty()) {
+            return notatPerqindjet.get(0);
+        } else {
+            return new Double[0];
+        }
     }
 }
