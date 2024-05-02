2d7d958af560818f769877a446e56948d1fd0a0e
medi <beqiriabdusamed@gmail.com>
2024-05-02 13:31:47 +0200

    pasna bo shum ndryshime qe pasna harru me i bo commit sorry amo funksionale krejt jon me sa di

--- frontend/ums/src/pages/dashboard.jsx
+++ frontend/ums/src/pages/dashboard.jsx
@@ -1,4 +1,4 @@
-import {React } from 'react'
+import { React } from 'react'
 import { Card } from "@mui/material";
 import { Link } from "react-router-dom";
 import { tokens } from '../theme';
@@ -7,31 +7,36 @@ import { useTheme } from '@mui/material'
 function dashboard() {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
+    console.log(colors.primary[400]);
     return (
         <>
             <div className='pl-10 pt-10'>
                 <div className='flex flex-row gap-4'>
 
-                <Link to="../Fakulteti">
-                    <Card className="w-72 h-48" sx={{background: colors.primary[500]}}>
-                        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                             Fakulteti
-                        </h5>
-                        <p className="font-normal text-gray-700 dark:text-gray-400">
-                            veq testim
-                        </p>
-                    </Card>
-                  </Link>
-                  <Link to="../Departamenti">
-                    <Card  className="w-72 h-48">
-                        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                            Departamenti
-                        </h5>
-                        <p className="font-normal text-gray-700 dark:text-gray-400">
-                            veq testim
-                        </p>
-                    </Card>
-                 </Link>
+                    <Link to="../Fakulteti">
+                        <Card className="w-72 h-48" sx={{background: colors.primary[400]}}>
+                            <div className='p-2'>
+                                <h5 className="text-2xl font-bold tracking-tight" sx={{colors: colors.gray[700]}}>
+                                    Fakulteti
+                                </h5>
+                                <p className="font-normal text-gray-700 dark:text-gray-400">
+                                    veq testim
+                                </p>
+                            </div>
+                        </Card>
+                    </Link>
+                    <Link to="../Departamenti">
+                        <Card className="w-72 h-48" sx={{background: colors.primary[400]}}>
+                            <div className='p-2'>
+                                <h5 className="text-2xl font-bold tracking-tight " sx={{colors: colors.gray[700]}}>
+                                    Departamenti
+                                </h5>
+                                <p className="font-normal text-gray-700 dark:text-gray-400">
+                                    veq testim
+                                </p>
+                            </div>
+                        </Card>
+                    </Link>
                 </div>
             </div>
         </>
