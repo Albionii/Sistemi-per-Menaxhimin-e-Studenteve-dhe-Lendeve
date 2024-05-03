57dbc4df6c9c261ec0e5bd5f20d0615ab08e620c
DrenSylaj <ds68258@ubt-uni.net>
2024-05-03 20:26:41 +0200

    Small fix
    File name lowerCase to upperCase , merge error.

--- frontend/ums/src/pages/home.jsx
+++ frontend/ums/src/pages/home.jsx
@@ -36,7 +36,7 @@ const Home = () => {
             aliquip ex ea commodo consequat. Duis aute irure dolor in
             reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum."
+            culpa qui officia deserunt mollet anim id est laborum."
           </h1>
         </Box>
         {/* ROW 2 */}
