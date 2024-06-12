c796beb3c6eded8c963deb61e7d4b27f0661f09f
Abdusamed Beqiri <beqiriabdusamed@gmail.com>
2024-06-12 13:22:39 +0200

    Albioni the Bug hunter helped me identify a bug

--- frontend/ums/src/components/RedirectByRole.jsx
+++ frontend/ums/src/components/RedirectByRole.jsx
@@ -3,7 +3,9 @@ import Home from '../pages/home'
 import CrudCategories from '../pages/CrudCategories'
 import { useNavigate } from 'react-router-dom';
 
-function RedirectByRole({token,user, role }) {
+function RedirectByRole({ token, role }) {
+    // console.log(token);
+    // console.log(role);
     const navigate = useNavigate();
     if (role === "ROLE_ADMIN") {
         navigate("/cruds", { replace: true })
@@ -16,7 +18,7 @@ function RedirectByRole({token,user, role }) {
     } else {
         navigate("/", { replace: true })
         return (
-            <Home token={token} user={user}/>
+            <Home token={token} user={role}/>
         )
     }
 }
